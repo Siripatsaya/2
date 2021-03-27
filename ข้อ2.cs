b24ca8159f1bb0d0ff7559eeb37652d1cb9923cb
LAPTOP-OAPN857L\SUN <siripatsaya.d@mail.kmutt.ac.th>
2021-03-28 04:11:46 +0700

    ข้อ2

---
+++ ข้อ2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int x = a;
+            int y = b;
+
+            while (x < y || x > y)
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+            }
+            if (x == y)
+            {
+                Console.Write("{0}", x);
+            }
+            else
+            {
+                Console.Write("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
